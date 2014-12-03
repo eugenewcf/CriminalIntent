f8071b82804eaa339f5fc620a98a34268da0e0f3
Eugene Wong <eugene.wong@razerzone.com>
2014-12-03 17:04:19 +0800

    [chapter-08] Challenge Completed

--- app/src/main/java/android/bignerdranch/com/criminalintent/CrimeFragment.java
+++ app/src/main/java/android/bignerdranch/com/criminalintent/CrimeFragment.java
@@ -5,15 +5,21 @@ import android.support.annotation.Nullable;
 import android.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,6 +49,21 @@ public class CrimeFragment extends Fragment {
                 // This one too
             }
         });
+
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+//        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setText( DateFormat.format("EEEE, MMM dd , yyyy", mCrime.getDate()));
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // Set the crime's solved property
+                mCrime.setSolved(isChecked);
+            }
+        });
+
 
         return v;
     }
