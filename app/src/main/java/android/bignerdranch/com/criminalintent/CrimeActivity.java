ffe9d68ac80fe67e3a8a0aa8a217b2eaf36eaab3
Eugene Wong <eugene.wong@razerzone.com>
2014-12-04 18:15:05 +0800

    [chapter-09] An Abstract Activity Class

--- app/src/main/java/android/bignerdranch/com/criminalintent/CrimeActivity.java
+++ app/src/main/java/android/bignerdranch/com/criminalintent/CrimeActivity.java
@@ -1,12 +1,9 @@
 package android.bignerdranch.com.criminalintent;
 
-import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class CrimeActivity extends ActionBarActivity {
@@ -14,7 +11,7 @@ public class CrimeActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
+        setContentView(R.layout.activity_fragment);
 
         FragmentManager fm = getFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
